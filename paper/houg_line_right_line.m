0c16f261adc061c065f2385610548c561de09e66
WeigangZhu <Zhu_Dean@outlook.com>
2018-04-27 10:42:25 +0800

    plot the pic if test the left or right line

---
+++ paper/houg_line_right_line.m
@@ -0,0 +1,105 @@
+clear,clc
+data = load('./hough/hough_line_parameters.txt');
+
+figure(1)
+X = reshape(data(1:780,2),[30,26]);
+Y = reshape(data(1:780,3),[30,26]);
+Z = reshape(data(1:780,10),[30,26]);
+
+% plot3(X, Y, Z, 'r.')
+mesh(X,Y,Z, 'linewidth', 1)
+h1 = xlabel('$$MinLength$$');
+h2 = ylabel('$$LineGap$$');
+h3 = zlabel('$$IsRight$$');
+grid on
+set([h1, h2, h3],'interpreter','latex');
+set([h1, h2, h3],'FontName','Times New Roman','Color','Black', 'FontSize',10.5);
+set(gca, 'FontName','Times New Roman','FontSize',10.5)
+grid on
+
+%%
+figure(2)
+X2 = reshape(data(781:780*2,2),[30,26]);
+Y2 = reshape(data(781:780*2,3),[30,26]);
+Z2 = reshape(data(781:780*2,10),[30,26]);
+
+% plot3(X, Y, Z, 'r.')
+mesh(X2,Y2,Z2, 'linewidth', 1)
+h1 = xlabel('$$MinLength$$');
+h2 = ylabel('$$LineGap$$');
+h3 = zlabel('$$IsRight$$');
+grid on
+set([h1, h2, h3],'interpreter','latex');
+set([h1, h2, h3],'FontName','Times New Roman','Color','Black', 'FontSize',10.5);
+set(gca, 'FontName','Times New Roman','FontSize',10.5)
+grid on
+
+%%
+figure(3)
+X3 = reshape(data(780*2+1:780*3,2),[30,26]);
+Y3 = reshape(data(780*2+1:780*3,3),[30,26]);
+Z3 = reshape(data(780*2+1:780*3,10),[30,26]);
+
+% plot3(X, Y, Z, 'r.')
+mesh(X3,Y3,Z3, 'linewidth', 1)
+h1 = xlabel('$$MinLength$$');
+h2 = ylabel('$$LineGap$$');
+h3 = zlabel('$$IsRight$$');
+grid on
+set([h1, h2, h3],'interpreter','latex');
+set([h1, h2, h3],'FontName','Times New Roman','Color','Black', 'FontSize',10.5);
+set(gca, 'FontName','Times New Roman','FontSize',10.5)
+grid on
+
+%%
+figure(4)
+X4 = reshape(data(780*3+1:780*4,2),[30,26]);
+Y4 = reshape(data(780*3+1:780*4,3),[30,26]);
+Z4 = reshape(data(780*3+1:780*4,10),[30,26]);
+
+% plot3(X, Y, Z, 'r.')
+mesh(X4,Y4,Z4, 'linewidth', 1)
+h1 = xlabel('$$MinLength$$');
+h2 = ylabel('$$LineGap$$');
+h3 = zlabel('$$IsRight$$');
+grid on
+set([h1, h2, h3],'interpreter','latex');
+set([h1, h2, h3],'FontName','Times New Roman','Color','Black', 'FontSize',10.5);
+set(gca, 'FontName','Times New Roman','FontSize',10.5)
+grid on
+
+%%
+figure(5)
+X5 = reshape(data(780*4+1:780*5,2),[30,26]);
+Y5 = reshape(data(780*4+1:780*5,3),[30,26]);
+Z5 = reshape(data(780*4+1:780*5,10),[30,26]);
+
+% plot3(X, Y, Z, 'r.')
+mesh(X5,Y5,Z5, 'linewidth', 1)
+h1 = xlabel('$$MinLength$$');
+h2 = ylabel('$$LineGap$$');
+h3 = zlabel('$$IsRight$$');
+grid on
+set([h1, h2, h3],'interpreter','latex');
+set([h1, h2, h3],'FontName','Times New Roman','Color','Black', 'FontSize',10.5);
+set(gca, 'FontName','Times New Roman','FontSize',10.5)
+grid on
+
+%%
+figure(6)
+X6 = reshape(data(780*5+1:780*6,2),[30,26]);
+Y6 = reshape(data(780*5+1:780*6,3),[30,26]);
+Z6 = reshape(data(780*5+1:780*6,10),[30,26]);
+
+% plot3(X, Y, Z, 'r.')
+mesh(X6,Y6,Z6, 'linewidth', 1)
+h1 = xlabel('$$MinLength$$');
+h2 = ylabel('$$LineGap$$');
+h3 = zlabel('$$IsRight$$');
+grid on
+set([h1, h2, h3],'interpreter','latex');
+set([h1, h2, h3],'FontName','Times New Roman','Color','Black', 'FontSize',10.5);
+set(gca, 'FontName','Times New Roman','FontSize',10.5)
+grid on
+
+%%
